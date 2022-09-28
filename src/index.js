19fc76df1ae190e4869981482b855acfafce0475
Bikesh <bikeshchaudhary1211@gmail.com>
2022-09-28 21:34:52 +0545

    initlize project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const port = process.env.PORT || 3001;
+const path = require("path");
+
+const app = express();
+const publicDirPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirPath))
+
+app.get("/", (req, res) => {
+  res.send("Hello Server ");
+});
+
+app.listen(port, () => {
+  console.log("Server Is Running on " + port);
+});
