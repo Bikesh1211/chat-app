764e6f72e274725e9390ef6ff4e926fceed9db46
Bikesh <bikeshchaudhary1211@gmail.com>
2022-10-06 17:24:32 +0545

    add socket io

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ const express = require("express");
 const port = process.env.PORT || 3001;
 const path = require("path");
 
+const http = require("http");
+const socketio = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const publicDirPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirPath));
+
+io.on("connection", () => {
+  console.log("web Socket connection!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello Server ");
 });
 
-app.listen(port, () => {
-  console.log("Server Is Running on " + port);
+server.listen(port, () => {
+  console.log("Server Is Running on " + "http://localhost:" + port);
 });
